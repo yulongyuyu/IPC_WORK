7277c3cb7a8b1953320bef30efd030a00cbadb99
yulongyuyu <wuyulongqaq.gmail.com>
2024-01-23 23:17:12 +0800

    ipcwork_1_23

--- IPC17_C_work_2/USER/main.c
+++ IPC17_C_work_2/USER/main.c
@@ -9,9 +9,15 @@
 #include "OLED.h"
 #include "IIC1.h"
 /*************
+<<<<<<< HEAD
 ¼ÇµÃÌí¼ÓÍ·ÎÄ¼þºÍ³õÊ¼»¯
 *************/
 int psc=8400;//Ô¤·ÖÆµÏµÊý
+=======
+è®°å¾—æ·»åŠ å¤´æ–‡ä»¶å’Œåˆå§‹åŒ–
+*************/
+int psc=8400;//é¢„åˆ†é¢‘ç³»æ•°
+>>>>>>> 76e7c77 (ipcwork_1_23)
 typedef struct tm
 {
 	int sec ;
@@ -20,6 +26,7 @@ typedef struct tm
 	
 }tm;
 tm mytime = {.sec = 0, .min = 0, .hour = 0};
+<<<<<<< HEAD
 void OLED_Show( tm  *pthis)
 {
 	OLED_ShowNum(1,1,pthis->hour);
@@ -27,6 +34,14 @@ void OLED_Show( tm  *pthis)
 	OLED_ShowNum(3,3,pthis->sec);
 }
 void display_sec(tm *pthis)
+=======
+
+/* 
+	å¯¹è®¡æ•°è¿›è¡Œå¤„ç†
+	å‚æ•°  tm *pthis åœ°å€
+*/
+void handle_sec(tm *pthis)
+>>>>>>> 76e7c77 (ipcwork_1_23)
 {
 	if (pthis->sec ==60)
 	{
@@ -42,8 +57,22 @@ void display_sec(tm *pthis)
 void tm_sec(tm *pthis)
 {
 	pthis->sec++;
+<<<<<<< HEAD
 }	
 	
+=======
+}
+
+/*
+	OLEDæ˜¾ç¤ºæ—¶é—´å‡½æ•°
+*/	
+void OLED_Show(tm *pthis)
+{
+	OLED_ShowNum(1,1,pthis->hour);
+	OLED_ShowNum(2,2,pthis->min);
+	OLED_ShowNum(3,3,pthis->sec);
+}	
+>>>>>>> 76e7c77 (ipcwork_1_23)
 
 
 int main(void)
@@ -54,11 +83,16 @@ int main(void)
 	OLED_Init();
 	
   while(1){
+<<<<<<< HEAD
 	  display_sec(&mytime);
+=======
+	  handle_sec(&mytime);
+>>>>>>> 76e7c77 (ipcwork_1_23)
 	  OLED_Show(&mytime);
    }
  }
 /******
+<<<<<<< HEAD
 ¶¨Ê±Æ÷ÖÐ¶Ï»Øµ÷º¯Êý
 ******/
  void TIM3_IRQHandler(void)//ÃëÖÐ¶ÏÒ»´Î
@@ -82,5 +116,18 @@ int main(void)
 			TIM_ClearITPendingBit(TIM4,TIM_IT_Update);
 		 }
  }
+=======
+å®šæ—¶å™¨ä¸­æ–­å›žè°ƒå‡½æ•°
+******/
+ void TIM3_IRQHandler(void)//ç§’ä¸­æ–­ä¸€æ¬¡
+ {
+	 if(TIM_GetITStatus(TIM3,TIM_IT_Update)==SET)
+		{
+			tm_sec(&mytime);
+	 		TIM_ClearITPendingBit(TIM3,TIM_IT_Update);
+		}
+ }
+
+>>>>>>> 76e7c77 (ipcwork_1_23)
 
  

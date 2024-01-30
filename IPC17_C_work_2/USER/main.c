b8f105d4a33d723231eb02457bfd40f0c5b46d97
yulongyuyu <wuyulongqaq.gmail.com>
2024-01-30 23:06:40 +0800

    main

--- IPC17_C_work_2/USER/main.c
+++ IPC17_C_work_2/USER/main.c
@@ -1,133 +1,72 @@
 #include "stm32f4xx.h"
 #include "tim.h" 
-#include "delay.h"
-#include "led.h"
-#include "bee.h"
-#include "key.h"
-#include "usart.h"
-#include "string.h"
 #include "OLED.h"
-#include "IIC1.h"
+
 /*************
-<<<<<<< HEAD
-�ǵ�����ͷ�ļ��ͳ�ʼ��
-*************/
-int psc=8400;//Ԥ��Ƶϵ��
-=======
 记得添加头文件和初始化
 *************/
-int psc=8400;//预分频系数
->>>>>>> 76e7c77 (ipcwork_1_23)
-typedef struct tm
-{
-	int sec ;
-	int min ;
-	int hour ;
-	
-}tm;
-tm mytime = {.sec = 0, .min = 0, .hour = 0};
-<<<<<<< HEAD
-void OLED_Show( tm  *pthis)
-{
-	OLED_ShowNum(1,1,pthis->hour);
-	OLED_ShowNum(2,2,pthis->min);
-	OLED_ShowNum(3,3,pthis->sec);
-}
-void display_sec(tm *pthis)
-=======
+#define PSC 8400 // 预分频系数
 
-/* 
-	对计数进行处理
-	参数  tm *pthis 地址
-*/
-void handle_sec(tm *pthis)
->>>>>>> 76e7c77 (ipcwork_1_23)
-{
-	if (pthis->sec ==60)
-	{
-		pthis->min++;
-		pthis->sec = 0;
-	}
-	if (pthis->min ==60)
-	{
-		pthis->hour ++;
-		pthis->min =0;
-	}
-}
-void tm_sec(tm *pthis)
-{
-	pthis->sec++;
-<<<<<<< HEAD
-}	
-	
-=======
+typedef struct {
+    int sec;
+    int min;
+    int hour;
+} TimeStruct;
+
+typedef struct {
+    TimeStruct* time;
+    void (*show)(TimeStruct*);
+    void (*increment)(TimeStruct*);
+} Clock;
+
+TimeStruct mytime = {.sec = 0, .min = 0, .hour = 0};
+Clock myClock; 
+
+void OLED_ShowTime(TimeStruct* time) {
+    OLED_ShowNum(1, 1, time->hour);
+    OLED_ShowNum(2, 2, time->min);
+    OLED_ShowNum(3, 3, time->sec);
 }
 
-/*
-	OLED显示时间函数
-*/	
-void OLED_Show(tm *pthis)
-{
-	OLED_ShowNum(1,1,pthis->hour);
-	OLED_ShowNum(2,2,pthis->min);
-	OLED_ShowNum(3,3,pthis->sec);
-}	
->>>>>>> 76e7c77 (ipcwork_1_23)
+void IncrementTime(TimeStruct* time) {
+    time->sec++;
+    if (time->sec == 60) {
+        time->min++;
+        time->sec = 0;
+    }
+    if (time->min == 60) {
+        time->hour++;
+        time->min = 0;
+    }
+}
+
+Clock CreateClock(TimeStruct* time) {
+    Clock clock;
+    clock.time = time;
+    clock.show = OLED_ShowTime;
+    clock.increment = IncrementTime;
+    return clock;
+}
 
 
-int main(void)
-{	
-	
-	led_init();
-	TIM3_Init(10000,psc);//84000000/8400/10000
-	OLED_Init();
-	
-  while(1){
-<<<<<<< HEAD
-	  display_sec(&mytime);
-=======
-	  handle_sec(&mytime);
->>>>>>> 76e7c77 (ipcwork_1_23)
-	  OLED_Show(&mytime);
-   }
- }
-/******
-<<<<<<< HEAD
-��ʱ���жϻص�����
-******/
- void TIM3_IRQHandler(void)//���ж�һ��
- {
-	 if(TIM_GetITStatus(TIM3,TIM_IT_Update)==SET)
-		{
-		tm_sec(&mytime);
-	 		TIM_ClearITPendingBit(TIM3,TIM_IT_Update);
-		}
- }
- 
- 
- 
- 
- 
-  void TIM4_IRQHandler(void)
- {		 
-	 if(TIM_GetITStatus(TIM4,TIM_IT_Update)==SET)
-		 {
+int main(void) {
+    led_init();
+    TIM3_Init(10000, PSC); // 84000000/8400/10000
+    OLED_Init();
+
+    myClock = CreateClock(&mytime);
+
+    while (1) {
+		myClock.show(&mytime);
+    }
+}
+
+void TIM3_IRQHandler(void) {
+    if (TIM_GetITStatus(TIM3, TIM_IT_Update) == SET) {
 		
-			TIM_ClearITPendingBit(TIM4,TIM_IT_Update);
-		 }
- }
-=======
-定时器中断回调函数
-******/
- void TIM3_IRQHandler(void)//秒中断一次
- {
-	 if(TIM_GetITStatus(TIM3,TIM_IT_Update)==SET)
-		{
-			tm_sec(&mytime);
-	 		TIM_ClearITPendingBit(TIM3,TIM_IT_Update);
-		}
- }
+        myClock.increment(&mytime);
 
->>>>>>> 76e7c77 (ipcwork_1_23)
-
+        TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
+    }
+}
  

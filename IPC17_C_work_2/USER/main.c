a6b3865d41c6474e5d674a92bd07ad869a81f311
yulongyuyu <wuyulongqaq.gmail.com>
2024-01-31 00:59:40 +0800

    1_31

--- IPC17_C_work_2/USER/main.c
+++ IPC17_C_work_2/USER/main.c
@@ -7,28 +7,25 @@
 *************/
 #define PSC 8400 // 预分频系数
 
-typedef struct {
+typedef struct Clock Clock;
+
+typedef struct Clock {
     int sec;
     int min;
     int hour;
-} TimeStruct;
-
-typedef struct {
-    TimeStruct* time;
-    void (*show)(TimeStruct*);
-    void (*increment)(TimeStruct*);
+    void (*show)(Clock *);
+    void (*increment)(Clock *);
 } Clock;
 
-TimeStruct mytime = {.sec = 0, .min = 0, .hour = 0};
-Clock myClock; 
+Clock *myClock;
 
-void OLED_ShowTime(TimeStruct* time) {
+void OLED_ShowTime(Clock *time) {
     OLED_ShowNum(1, 1, time->hour);
     OLED_ShowNum(2, 2, time->min);
     OLED_ShowNum(3, 3, time->sec);
 }
 
-void IncrementTime(TimeStruct* time) {
+void IncrementTime(Clock *time) {
     time->sec++;
     if (time->sec == 60) {
         time->min++;
@@ -40,33 +37,32 @@ void IncrementTime(TimeStruct* time) {
     }
 }
 
-Clock CreateClock(TimeStruct* time) {
+Clock CreateClock() {
     Clock clock;
-    clock.time = time;
+    clock.sec = 0;
+    clock.min = 0;
+    clock.hour = 0;
     clock.show = OLED_ShowTime;
     clock.increment = IncrementTime;
     return clock;
 }
-
-
-int main(void) {
-    led_init();
-    TIM3_Init(10000, PSC); // 84000000/8400/10000
-    OLED_Init();
-
-    myClock = CreateClock(&mytime);
-
-    while (1) {
-		myClock.show(&mytime);
-    }
-}
-
+  
 void TIM3_IRQHandler(void) {
     if (TIM_GetITStatus(TIM3, TIM_IT_Update) == SET) {
-		
-        myClock.increment(&mytime);
-
+        myClock->increment(myClock);
         TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
     }
 }
- 
+
+int main(void) {
+    TIM3_Init(10000, PSC); // 84000000/8400/10000
+    OLED_Init();
+    Clock myClockInstance = CreateClock();
+    myClock = &myClockInstance;  
+    while (1) {
+        myClock->show(myClock);
+    }
+}
+
+
+
